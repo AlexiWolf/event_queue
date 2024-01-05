f5a370095f48136154a05a6c248258187fe09376
AlexiWolf <alexi@wolvesin.space>
2024-01-04 21:24:39 -0600

    Removed `HasEventSender` trait

--- src/event_queue.rs
+++ src/event_queue.rs
@@ -1,15 +1,7 @@
-use std::sync::Arc;
-
 /// A generic, FIFO, MPSC event queue.
-pub trait EventQueue<E>: HasEventSender<E> {
+pub trait EventQueue<E> {
     /// Returns the next event in the queue.
     fn next_event(&mut self) -> Option<E>;
-}
-
-/// A type which has an [`EventSender`].
-pub trait HasEventSender<E> {
-    /// Creates a new [`EventSender`].
-    fn event_sender(&self) -> Arc<dyn EventSender<E>>;
 }
 
 /// The event-sending half of an [`EventQueue`].

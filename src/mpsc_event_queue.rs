2dcea609c5a0a04c7f045e4efce82528b2467b48
AlexiWolf <alexi@wolvesin.space>
2024-01-04 21:58:36 -0600

    Changed expectation of init to use `mpsc_event_queue()` fn

--- src/mpsc_event_queue.rs
+++ src/mpsc_event_queue.rs
@@ -56,7 +56,7 @@ mod event_queue_tests {
 
     #[test]
     pub fn should_send_and_receive_events() {
-        let (event_sender, mut event_queue) = MpscEventReceiver::new();
+        let (event_sender, mut event_queue) = mpsc_event_queue();
 
         event_sender.send_event(0).unwrap();
 
@@ -65,7 +65,7 @@ mod event_queue_tests {
 
     #[test]
     pub fn should_send_events_and_receive_events_across_threads() {
-        let (event_sender, mut event_queue) = MpscEventReceiver::new();
+        let (event_sender, mut event_queue) = mpsc_event_queue();
 
         event_sender.send_event(0).unwrap();
         let thread_sender = event_sender.clone();
@@ -83,7 +83,7 @@ mod event_queue_tests {
 
     #[test]
     pub fn should_flush_empty_list_if_there_are_no_events() {
-        let (_event_sender, mut event_queue) = MpscEventReceiver::<u32>::new();
+        let (_event_sender, mut event_queue) = mpsc_event_queue::<i32>(); 
 
         assert!(event_queue.next_event().is_none());
     }
